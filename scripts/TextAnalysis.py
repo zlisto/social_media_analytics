fc1e3327289fd576205385dad1cafcf4443c6f98
Tauhid Zaman <zlisto@gmail.com>
2022-01-29 22:49:39 -0500

    textanalysis

--- scripts/TextAnalysis.py
+++ scripts/TextAnalysis.py
@@ -7,7 +7,27 @@ def clean_tweet(elem):
     s1 =re.sub(r"([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?|u2026|u2019|u2019s|u2014|amp", "", elem.lower())
     return s1.strip()
 
+def word2vec_embedding(list_of_docs:List[str], 
+                       model:gensim.models.keyedvectors.Word2VecKeyedVectors)-> np.ndarray:
+    features = []
 
+    for tokens in list_of_docs:
+        zero_vector = np.zeros(model.vector_size)
+        vectors = []
+        for token in tokens:
+            if token in model.wv:
+                try:
+                    vectors.append(model.wv[token])
+                except KeyError:
+                    continue
+        if vectors:
+            vectors = np.asarray(vectors)
+            avg_vec = vectors.mean(axis=0)
+            features.append(avg_vec)
+        else:
+            features.append(zero_vector)
+    return np.array(features)
+    
 #measure sentiment of text from a pre-trained transformer model+tokenizer
 def sentiment_classifier(text,model,tokenizer):
     inputs = tokenizer.encode_plus(text, return_tensors='pt', add_special_tokens=True)
