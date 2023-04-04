e8673de423a3dae395353f9c845088f256b7f3a8
zlisto <tauhid.zaman@yale.edu>
2023-04-04 00:06:15 -0400

    engagement lec 16 and text analytics

--- scripts/TextAnalysis.py
+++ scripts/TextAnalysis.py
@@ -86,3 +86,26 @@ def sample_words(input_text,generator,nsamples):
     df_freq.sort_values(by = 'freq', ascending = False, inplace = True)
     df_freq.head()
     return df_freq
+
+def engagement_estimate(df):
+    data = df[['like_count','retweet_count','reply_count','impression_count','keyword']].groupby(by = 'keyword').sum()
+
+    nlike = data.like_count.sum()
+    nretweet = data.retweet_count.sum()
+    nreply = data.reply_count.sum()
+    
+    vc = data.impression_count
+    nc = data.like_count + data.retweet_count + data.reply_count
+    n = nlike + nretweet + nreply
+
+    alphac = nlike/n * nc/vc
+    df_alpha = alphac.to_frame(name='alpha').reset_index(drop=False)
+    
+  
+    beta_like = nlike/nlike
+    beta_retweet = nretweet/nlike
+    beta_reply = nreply/nlike
+    
+    df_beta = pd.DataFrame({'interaction':['like','retweet','reply'],
+                             'beta':[beta_like, beta_retweet, beta_reply]})
+    return df_alpha, df_beta
